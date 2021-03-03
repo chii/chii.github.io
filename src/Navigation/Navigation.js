3ab57d4ac7c9ae32bb0fba328591159bb07f28d3
Chii <chihironitasaka@gmail.com>
2021-03-03 14:00:35 -0800

    modify Navigation func name

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -3,14 +3,16 @@ import { Link } from 'react-router-dom';
 
 import './Navigation.css';
 
-const Nav = () => {
+const Navigation = () => {
     return(
+        <>
         <nav className="Navigation">
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
           <Link to="/work">Work</Link>
         </nav>
+        </>
     )
 }
 
-export default Nav
+export default Navigation
